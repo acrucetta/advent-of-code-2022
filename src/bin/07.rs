6e5b6e9526c5765526a589904d8cda443508defc
Andres Crucetta Nieto <acrucettanieto@gmail.com>
2022-12-07 16:33:07 -0600

    Didn't work with my own tree structure, trying to use Rust's tree methods

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -27,13 +27,16 @@ Methods
     - We will need to print out the name of the directory and its size
 */
 
-use std::collections::{HashMap, HashSet};
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::rc::Rc;
+use std::vec;
 
-struct Node {
+pub struct Node {
     name: String,
     size: u32,
-    children: Vec<Node>,
-    parent: Option<Box<Node>>,
+    children: Vec<Rc<RefCell<Node>>>,
+    parent: Option<Rc<RefCell<Node>>>,
     node_type: FileType,
 }
 
@@ -42,47 +45,77 @@ enum FileType {
     File,
 }
 
+impl Clone for FileType {
+    fn clone(&self) -> Self {
+        match self {
+            FileType::Directory => FileType::Directory,
+            FileType::File => FileType::File,
+        }
+    }
+}
+
 impl Node {
-    fn new(name: String, size: u32) -> Node {
+    fn new(name: String, size: u32, node_type: FileType) -> Node {
         Node {
             name,
             size,
-            children: Vec::new(),
+            children: vec![],
             parent: None,
+            node_type: node_type,
         }
     }
 
-    fn add_child(&mut self, child: Node) {
-        self.children.push(child);
+    fn clone(&self) -> Node {
+        Node {
+            name: self.name.clone(),
+            size: self.size,
+            children: self.children.iter().map(|child| child.clone()).collect(),
+            parent: None,
+            node_type: self.node_type.clone(),
+        }
     }
 
-    fn add_parent(&mut self, parent: Node) {
-        self.parent = Some(Box::new(parent));
+    fn add_child(&mut self, new_node: Rc<RefCell<Node>>) {
+        self.children.push(new_node);
     }
 
     fn get_size(&self) -> u32 {
-        let mut size = self.size;
-        for child in &self.children {
-            size += child.get_size();
+        match self.node_type {
+            FileType::Directory => {
+                let mut size = 0;
+                for child in &self.children {
+                    size += child.borrow().get_size();
+                }
+                size
+            }
+            FileType::File => self.size,
         }
-        size
     }
 
-    fn get_name(&self) -> String {
-        self.name.clone()
+    pub fn go_to_root(&self) -> Option<Rc<RefCell<Node>>> {
+        if let Some(parent) = &self.parent {
+            return parent.borrow().go_to_root();
+        } else {
+            return Some(Rc::clone(&self.parent));
+        }
     }
 
-}
+    fn calculate_directory_sizes(&self) -> HashMap<&String, u32> {
+        // We will need to traverse the tree and find all directories that are at most N size
+        // we will do a BFS traversal
+        let mut queue = Vec::new();
+        let mut directory_sizes = HashMap::new();
+        queue.push(self);
 
-struct Tree {
-    root: Node,
-}
-
-impl Tree {
-    fn new() -> Tree {
-        Tree {
-            root: Node::new("root".to_string(), 0),
+        while !queue.is_empty() {
+            let node = queue.pop().unwrap();
+            let size = node.get_size();
+            directory_sizes.insert(&node.name, size);
+            for child in &node.children {
+                queue.push(child);
+            }
         }
+        directory_sizes
     }
 }
 
@@ -91,59 +124,85 @@ enum Command {
     Ls,
 }
 
-pub fn execute_command(input: &str, mut node: Node) -> Node {
+pub fn execute_command(input: &str, mut node: Rc<RefCell<Node>>) -> Node {
     // We will need to parse the cd and ls commands and execute them
     match input {
-        "cd /" => {
+        "/" => {
             // We will need to move to the root node
-        },
-        "cd .." => {
+            return_node = node.get_root();
+        }
+        ".." => {
             // We will need to move to the parent node
-        },
+            if let Some(parent) = node.parent {
+                return_node = *parent;
+            }
+        }
         _ => {
             // We will need to move to the child node
+            for child in &node.children {
+                if child.name == input {
+                    return_node = child.clone();
+                    break;
+                }
+            }
         }
     }
-    None
+    return_node
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    
-    let mut root_node = Node::new("/".to_string(), 0);
-    let mut listing_files = false;
+    let root_node = Rc::new(RefCell::new(Node::new(
+        "root".to_string(),
+        0,
+        FileType::Directory,
+    )));
+    let mut current = Rc::clone(&root_node);
 
     for line in input.lines() {
-        let mut command_parts : Vec<&str> = line.split_whitespace().collect();
-        
-        if command_parts[0] == "$" {
-            listing_files = false; // We will reset the listing_files flag
+        let mut command_parts: Vec<&str> = line.split_whitespace().collect();
 
+        if command_parts[0] == "$" {
             match command_parts[1] {
                 "cd" => {
                     // We will need to execute the cd command
                     let command = command_parts[2];
                     let node = execute_command(command, root_node);
                     root_node = node;
-                },
-                "ls" => {
-                    // We will need to execute the ls command
-                    listing_files = true;
                 }
+                "ls" => {
+                    continue;
+                }
+                _ => panic!("Invalid command: {}", command_parts[1]),
             }
         } else {
             // Add the file to the current node
             if command_parts[0] == "dir" {
                 let name = command_parts[1].to_string();
-                let child_node = Node{name: name, size: 0, children: Vec::new(), parent: None, node_type: FileType::Directory};
-                root_node.add_child(child_node);
+                let child_node = Rc::new(RefCell::new(Node::new(name, 0, FileType::Directory)));
+                current.borrow_mut().children.push(Rc::clone(&child_node));
+
+                let mut child_node = child_node.borrow_mut();
+                child_node.parent = Some(Rc::clone(&current));
             } else {
                 let size = command_parts[0].parse::<u32>().unwrap();
                 let name = command_parts[1].to_string();
-                let child_node = Node{name: name, size: size, children: Vec::new(), parent: None, node_type: FileType::File};
-                root_node.add_child(child_node);
+                let child_node = Rc::new(RefCell::new(Node::new(name, size, FileType::Directory)));
+                current.borrow_mut().children.push(Rc::clone(&child_node));
+
+                let mut child_node = child_node.borrow_mut();
+                child_node.parent = Some(Rc::clone(&current));
             }
+        }
     }
-    None
+    let directory_sizes = root_node.borrow().calculate_directory_sizes();
+    let max_size = 10000;
+    // Sum the sizes of all the directories that are at most 10000
+    let sum_size = directory_sizes
+        .iter()
+        .filter(|(_, size)| **size <= max_size)
+        .map(|(_, size)| size)
+        .sum();
+    Some(sum_size)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
